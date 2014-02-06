fa03e986f4e780cb1532413ed685864727ff15c3
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2014-02-06 22:51:08 +0100

    Integrate websocket handling into CabbageBot
    CabbageBot is an event emitter. Emitted events (aside from `error`) are
    `open`, when the bot connected to chat, `close` when the bot disconnected
    and is no longer listening, and `event` when an event is reported from
    chat.
    
    Actual bot handlers need to listen to `event` to get the actual events
    from the chat room.

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-var WebSocket = require('ws');
-
 var CabbageBot = require('./lib/cabbage');
 var delay = require('./lib/utils').delay;
 var openIdLogin = require('./lib/auth').openIdLogin;
@@ -22,8 +20,11 @@ function handleMessageEvent (e, cbg) {
 
 	if (e.content.indexOf('!!rabbit') > -1) {
 		cbg.send('I like rabbits!', e.room_id).then(function (msgId) {
-			console.log('Message sent: ' + msgId);
+			if (!msgId) {
+				throw new Error('Invalid id');
+			}
 
+			console.log('Message sent: ' + msgId);
 			return delay(5000).then(function () {
 				return cbg.edit('I *really* like rabbits!', msgId).then(function (msgId) {
 					console.log('Message edited: ' + msgId);
@@ -34,40 +35,30 @@ function handleMessageEvent (e, cbg) {
 }
 
 // start bot
-var cbg = new CabbageBot();
-openIdLogin(config.emailAddress, config.password).then(cbg.connect.bind(cbg)).then(function (fkey) {
-	// register SIGINT handler
-	process.on('SIGINT', function() {
-		console.log('Shutting down.');
-		cbg.leaveAll().then(function () {
-			process.exit();
-		});
-	});
-
-	// join chat room
-	return cbg.join(config.roomId).then(function (wsAddress) {
-		var ws = new WebSocket(wsAddress + '?l=0', { origin: 'http://chat.stackoverflow.com' });
-		ws.on('error', function (err) {
-			console.log(err);
-		});
-		ws.on('open', function () {
-			console.log('Websocket opened.');
-		});
-		ws.on('message', function (message, flags) {
-			var data = JSON.parse(message);
-			var room = data.r6;
-
-			if (room.e && room.t != room.d) {
-				room.e.forEach (function (e) {
-					if (e.event_type == 1) {
-						handleMessageEvent(e, cbg);
-					}
-					else {
-						console.log(e);
-					}
-				});
-			}
-		});
-	});
-})
+var cbg = new CabbageBot(config.roomId);
+openIdLogin(config.emailAddress, config.password)
+.then(cbg.connect.bind(cbg))
+.then(cbg.listen.bind(cbg))
 .catch(console.error);
+
+// SIGINT event handler
+process.on('SIGINT', cbg.quit.bind(cbg));
+
+// event handler
+cbg.once('open', function () {
+	console.log('Connection established.');
+});
+cbg.once('close', function () {
+	console.log('Connection closed.');
+});
+cbg.on('error', function (err) {
+	console.error(err);
+})
+cbg.on('event', function (evt) {
+	if (evt.event_type == 1) {
+		handleMessageEvent(evt, cbg);
+	}
+	else {
+		console.log(evt);
+	}
+});
